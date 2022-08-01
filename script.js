0c0c979ff7e49d1867040a5df545e3edc2833437
GokulJD <106479531+GokulJD@users.noreply.github.com>
2022-08-01 09:05:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var yourname = prompt("What is your name");
+var letter1 = yourname.slice(0,1);
+var restletters = yourname.slice(1,10);
+alert("Hi "+ letter1.toUpperCase()+restletters+" welcome here");
